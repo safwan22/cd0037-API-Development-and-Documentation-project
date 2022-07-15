6347a9d46d2320aab5036c6a7b56d8d8f64335ed
Safwan Hussaini <safwanhussaini70@gmail.com>
2022-07-15 22:47:31 +0000

    rfnndf

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -57,6 +57,12 @@ def paginate_questions(request, selection):
   page = request.args.get('page', 1, type=int)
   start = (page - 1) * QUESTIONS_PER_PAGE
   end = start + QUESTIONS_PER_PAGE
+  
+  questions = [question.format() for question in selection]
+  current_questions = questions[start:end]
+
+  return current_questions
+
 
  # GET QUESTIONS
 @app.route('/questions')
